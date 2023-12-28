0cc2ca45adce19b10d0b1cd26210bb90753cd9ee
Nayan Desai <ndmaratha01@gmail.com>
2023-12-28 06:37:51 +0530

    header css remaining

--- ShreeRam/src/Component/Header.jsx
+++ ShreeRam/src/Component/Header.jsx
@@ -1,9 +1,16 @@
+import
 const Header=()=>{
     return (
 
-        <>
-        <div>Header</div>
-        </>
+    <div>
+        <img src={} alt="logo" />
+        <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+        <button>Cart</button>
+        <button>Login</button>
+    </div>
     );
 }
 export default Header;
